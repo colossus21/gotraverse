abfe88bd6c1c37f16fa8b208bc5dfac9161ff3b8
Rafiul <rafiul2111@gmail.com>
2019-02-22 23:18:31 +0600

    Add Configuration File

---
+++ Config.go
@@ -0,0 +1,18 @@
+package gotraverse
+
+import "math"
+//Configuration constants
+const (
+	INF = math.MaxInt32
+	LOG_A_STAR_STEPS = true
+	LOG_A_STAR_DETAILS = false
+	LOG_GREEDY_STEPS = true
+	LOG_GREEDY_DETAILS = false
+	LOG_UCS_STEPS = true
+	LOG_UCS_DETAILS = false
+	LOG_DFS_STEPS = true
+	LOG_DFS_DETAILS = true
+	LOG_BFS_STEPS = true
+	LOG_BFS_DETAILS = true
+)
+
